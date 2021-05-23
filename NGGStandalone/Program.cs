c4df77e5e6d560ada5cad243f4dbec3543efaab2
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-05-23 16:43:57 +0100

    Added start of testing.

--- NGGStandalone/Program.cs
+++ NGGStandalone/Program.cs
@@ -6,7 +6,18 @@ namespace NGGStandalone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int num = rnd.Next(0, 100);
+            Console.Write("Please enter a number.\n>");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            if (num == userNumber)
+            {
+                Console.WriteLine("That's correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Number.");}
+            
         }
     }
 }
