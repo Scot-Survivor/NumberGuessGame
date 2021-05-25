d0abb83230369f2eea28c2763a16e383c4a3c6aa
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-05-25 15:31:15 +0100

    Finished the standalone app.

--- NGGStandalone/Program.cs
+++ NGGStandalone/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NGGStandalone
 {
@@ -6,18 +7,52 @@ namespace NGGStandalone
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Number Guess.\nYou must guess a number between 1 & 10. You win if you get 5 numbers correct.");
+            List<string> commandWords = new List<string> {"quit", "score"};
             Random rnd = new Random();
-            int num = rnd.Next(0, 100);
-            Console.Write("Please enter a number.\n>");
-            int userNumber = Convert.ToInt32(Console.ReadLine());
-            if (num == userNumber)
+            int score = 0;
+            int tries = 0;
+            while (true)
             {
-                Console.WriteLine("That's correct!");
+                var numberToGuess = rnd.Next(0, 10);
+                Console.Write(
+                    "Commands\n     Quit/quit: Quit the Program.\n     Score/score: Print your score.\nEnter command or your guess\n>");
+                string userInput = Console.ReadLine();
+                if (userInput != null && commandWords.Contains(userInput.ToLower()))
+                {
+                    userInput = userInput.ToLower();
+                    switch (userInput)
+                    {
+                        case "quit":
+                            return;
+                        case "score":
+                            Console.WriteLine($"Your score is: {score} You have tried: {tries} times.");
+                            break;
+                    }
+                }
+                else if (userInput != null)
+                {
+                    int userInputInt = Convert.ToInt32(userInput);
+                    if (numberToGuess == userInputInt)
+                    {
+                        tries++;
+                        Console.WriteLine($"That's correct! You have tried: {tries} times.");
+                        score++;
+                        if (score < 5) continue;
+                        Console.WriteLine("You Win!");
+                        return;
+                    }
+                    else
+                    {
+                        tries++;
+                        Console.WriteLine($"Wrong Number. You have tried: {tries} times.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter something!");
+                }
             }
-            else
-            {
-                Console.WriteLine("Wrong Number.");}
-            
         }
     }
 }
