3b92f69c57698310020080da696deca30dbd5fdc
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-05-26 15:56:56 +0100

    Added GameMessages Library, for classes shared between client/server

---
+++ GameMessages/GameMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameMessages
+{
+    public class ClientMessage
+    {
+        public int ClientId {get; set; }
+        public int GameId {get; set;}
+        public string Command { get; set; }
+        public DateTimeOffset TimeSent { get; set; }
+        public string SourceAddr { get; set; }
+        public string DestinationAddr { get; set; }
+    }
+}
